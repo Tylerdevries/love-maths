1e18b1458d3d32183ef8269d31cbdda9eabf43dd
Tylerdevries <tylerdevries365@gmail.com>
2022-01-28 23:06:29 +0000

    final touch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-/ Wait for the DOM to finish loading before running the game
+// Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
 document.addEventListener("DOMContentLoaded", function() {
